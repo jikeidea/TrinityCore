b8b1208f1fc617c3668c123884e0adda7b2ee88c
Aokromes <aokromes@gmail.com>
2019-03-20 22:22:24 +0100

    typo fix

--- sql/updates/world/4.3.4/2019_02_22_00_world.sql
+++ sql/updates/world/4.3.4/2019_02_22_00_world.sql
@@ -6,4 +6,4 @@ UPDATE `creature_template` SET `AIName`="SmartAI" WHERE `entry`=8891;
 DELETE FROM `player_factionchange_items` WHERE `alliance_id`=30622;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=14 AND `SourceGroup`=1043;
 DELETE FROM `conditions` WHERE `SourceTypeOrReferenceId`=15 AND `SourceGroup`=1043;
-UPDATE `smart_scripts` SET `action_param1`=170 WHERE `entryorguid` IN (-91106, -91107) AND `source_type`=0 AND `id`=3
+UPDATE `smart_scripts` SET `action_param1`=170 WHERE `entryorguid` IN (-91106, -91107) AND `source_type`=0 AND `id`=3;
